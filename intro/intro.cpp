aca0e59abee6b34c70387e23c2a4c0b17c308775
Abdul Wasay <106981654+abdulwasay0245@users.noreply.github.com>
2025-10-25 19:31:27 +0500

    Add introductory C++ program that prints a message

---
+++ intro/intro.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// This statement brings all names from the 'std' namespace into the global scope.
+using namespace std; 
+
+int main() {
+    // No need for 'std::' prefix now
+    cout << "Hello, C++ World!" << endl;
+    
+    return 0; 
+}
